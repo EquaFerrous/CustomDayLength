829298e820b8d086f8918a6aeea927c007423b3c
benbrooks15 <102412238+EquaFerrous@users.noreply.github.com>
2022-04-03 17:13:58 +0100

    Day / Night Value Change
    - Changed the values of default day and night to better reflect mob spawning times.

--- src/main/java/me/equaferrous/customdaylength/timemanager/TimeManager.java
+++ src/main/java/me/equaferrous/customdaylength/timemanager/TimeManager.java
@@ -19,8 +19,8 @@ public class TimeManager {
     private int nightAdvanceAmount;
     private int nightAdvanceDelay;
 
-    private final int defaultDayLength = 12000;
-    private final int defaultNightLength = 12000;
+    private final int defaultDayLength = 14000;
+    private final int defaultNightLength = 10000;
     private int customDayLength;
     private int customNightLength;
 
@@ -79,8 +79,12 @@ public class TimeManager {
 
     // Sets a new full-day length
     public void SetFullDayLength(double minutes) {
-        customDayLength = (int) ((minutes / 2) * 60 * 20);
-        customNightLength = (int) ((minutes / 2) * 60 * 20);
+        double dayRatio = (double) defaultDayLength / 24000;
+        double nightRatio = (double) defaultNightLength / 24000;
+        customDayLength = (int) ((minutes * 60 * 20) * dayRatio);
+        customNightLength = (int) ((minutes * 60 * 20) * nightRatio);
+        Bukkit.broadcastMessage(String.valueOf(customDayLength));
+        Bukkit.broadcastMessage(String.valueOf(customNightLength));
         UpdateDayValues();
         UpdateNightValues();
 
@@ -105,8 +109,8 @@ public class TimeManager {
         double nightLength = ((double) customNightLength / 20) / 60;
         //double standardNight = ((double) defaultNightLength / 20) / 60;
 
-        text += "[ Day length : "+ dayLength +" minutes ]";
-        text += "\n[ Night length : "+ nightLength +" minutes ]";
+        text += "[ Day length : "+ GetMinutesSeconds(dayLength) +" ]";
+        text += "\n[ Night length : "+ GetMinutesSeconds(nightLength) +" ]";
 
         recipient.sendMessage(ChatColor.GRAY + text);
     }
@@ -217,6 +221,7 @@ public class TimeManager {
 
     // Get the fractional parts of a decimal
     private int[] DecimalToFraction(long decimal) {
+        decimal = (long) (Math.floor(decimal * 100) / 100);
         String decimalText = Double.toString(Math.abs(decimal));
         int integerPlaces = decimalText.indexOf('.');
         int decimalPlaces = decimalText.length() - integerPlaces - 1;
@@ -230,11 +235,31 @@ public class TimeManager {
 
     // Returns if it is currently night
     private boolean CheckIfNight() {
-        return (overworld.getTime() > 12000);
+        long currentTime = overworld.getTime();
+        return (currentTime > 13000 && currentTime <= 23000);
     }
 
     // Returns whether the time has changed between day / night
     private boolean CheckForTimeChange() {
         return (CheckIfNight() != nightValues);
+    }
+
+
+    // Returns the inputted minutes as a string of minutes + seconds
+    private String GetMinutesSeconds(double minutes) {
+        String decimalText = Double.toString(Math.abs(minutes));
+        String[] splitText = decimalText.split("\\.");
+
+        String returnText;
+        if (splitText[1].equals("0")) {
+            returnText = splitText[0] +" minutes";
+        }
+        else {
+            double seconds = Double.parseDouble("0."+ splitText[1]);
+            int secondsText = (int) (Math.round(seconds * 60));
+            returnText = splitText[0] +" minutes, "+ secondsText +" seconds";
+        }
+
+        return returnText;
     }
 }
